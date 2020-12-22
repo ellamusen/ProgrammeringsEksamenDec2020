f9459bd2133073be87e4c910f2a4ea2ec7899eff
ellamusen <dani_1997@live.dk>
2020-12-22 09:58:04 +0100

    Added blankLine method

--- src/sample/SimpleTest.java
+++ src/sample/SimpleTest.java
@@ -1,8 +1,14 @@
 package sample;
 
+import javax.crypto.spec.PSource;
+
 public class SimpleTest {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my recipe-application-dummy");
+    }
+
+    public static void blankLine() {
+        System.out.println("\b");
     }
 }
